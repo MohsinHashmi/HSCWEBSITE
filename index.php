2e7bb7fbd7c8bb4340041225006a05fdbbd40edf
mohsin <maahhashmi47@gmail.com>
2022-12-23 15:09:12 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/main.css" >
+    <script src="js/new.js"></script>
+ 
+    <title>Document</title>
+   
+</head>
+<body>
+<header id="headers">
+    <div id="lo"> <img src="images/hsclogo.png" width="200px" height="200px" id="logo" ></div>
+<div>
+<ul><li><a href="#HMS" id="HM">HOME</a></li></ul>
+<ul><li><a href="#SRS" id="SR">SERVICES</a></li></ul>
+<ul><li><a href="#CLS" id="CL">CLIENTS</a></li></ul>
+<ul><li><a href="#ATS" id="AT">ABOUT US</a></li></ul>
+<ul><li><a href="#TMS" id="TM">Registration Section</a></li></ul>
+</div>
+</header>
+  <div id="HMS">
+    <!-- <iframe id="ifm" width="100%" height="600px" src="file:///E:/wamp64/www/hscwebsite/images/video.mp4?autoplay=1&mute=1" ></iframe> -->
+        <video id="ifm" width="100%" height="650px" autoplay><source src="images/video.mp4" type="video/mp4"></video>
+  </div>  
+  <h1 id="q">What Is Intellectual Property?</h1>
+    
+<div id="SRS">
+<div id="d1">
+    <div>
+<img src="images/tm.png" width="100%" height="100%" id="img1">
+        <h1 id="heads">Trademarks</h1>
+<div id="tmp">
+        <p>
+    While patents safeguard inventions, trademarks and
+service marks facilitate brand recognition by protecting
+names, logos, symbols, and other designs that exclusively
+identify a brand. Hassan Shaikh and Company helps clients
+implement trademark strategies to stand out in their
+industries.
+With backgrounds in chemistry, engineering, physics,
+computer science and other technical disciplines, our
+attorneys have experience handling a full range of
+trademark issues. We assist businesses in industries
+ranging from biochemistry to electrical engineering,
+manufacturing, automotive and more.
+    </p>
+    </div>
+    </div>
+</div>
+    <div id="d2">
+    <div></div>
+    
+    <img src="images/cr.jpg" width="100%" height="100%" id="img2">
+
+    <h1 id="heads">Copyright:</h1>
+    <div id="tmp">
+<p>
+Copyrights protect original creative expressions through
+artistic, written, musical, architectural, audiovisual, and
+digital works. A copyright grants you exclusive rights to
+reproduce, distribute, and sell your work for a limited
+time, but navigating complex copyright issues can be
+difficult without an experienced partner like Hassan
+Shaikh and Company.
+    </p>
+    </div>
+</div>
+    <div id="d3">
+    <div></div>
+    <img src="images/dsg2.jpg" width="100%" height="100%" id="img3">
+    
+    <h1 id="heads">Desings:</h1>
+    <div id="tmp">
+<p>
+Copyrights protect original creative expressions through
+artistic, written, musical, architectural, audiovisual, and
+digital works. A copyright grants you exclusive rights to
+reproduce, distribute, and sell your work for a limited
+time, but navigating complex copyright issues can be
+difficult without an experienced partner like Hassan
+Shaikh and Company.
+    </p>
+    </div>   
+</div>
+
+    <div id="d4">
+    <div></div>
+    <img src="images/img8.png" width="100%" height="100%" id="img4">
+
+    <h1 id="heads">Patents:</h1>
+    <div id="tmp">
+<p>
+Patents are crucial to protecting your inventions, innovations,
+and other intellectual property, but the legalities can be
+complex. With more than 17 years of experience handling all
+aspects of patent law, we understand how to help our clients
+establish market-leading positions through effective
+patenting strategies.
+Our patent attorneys and IP engineers have degrees in a
+broad spectrum of chemistry, engineering, physics, and
+computer science disciplines, and many have worked in those
+fields. We pair our academic know-how and technical
+acumen with practical, real-world experience to provide
+sophisticated legal counsel.
+    </p></div>    
+</div>
+  
+  </div>  
+  
+  <div id="CLS">
+    <div id="cld">
+<div id="cdd"><img src="images/cl1.png" width="200px" height="100px"></div>  
+        </div>
+  </div>  
+  <div id="ATS">
+  <div id="had">
+    
+  <h1 id="hh">History- <span id="hhh">The Evolution of HSCo<span></h1>
+  <br> 
+  <h2 id="hef">In 2003,when Hassan Shaikh and Company opened its doors, the latest buzz in intellectual property was hurdles in IP registration in Pakistan. Since then, our world, and the innovations that shape it, have changed immensely. Through it all, our mission has remained simple: provide a complete array of intellectual property legal services focused on solutions for your innovations, ideas, andcreativity. </h2>
+  <br> 
+  <br> 
+    <h3 id="heff">Technical Sophistication</h3>
+    <p id="pp">Our attorneys, patent agents, technical writers, and consultants represent a wide array of engineering and scientific backgrounds and specialties. Practical experience, academic know-how, and technical acumen put us on the same technological page as our clients, resulting in sophisticated and relevant advice on technical and business issues. We are consistently ranked nationally in the upper echelon of Intellectual Property Law firms plus we have an international network of associates and patent agents to provide globalintellectualproperty solutions.
+</p>
+<br> 
+<br> 
+
+<h3 id="heff">Effortless Communication</h3>
+    <p id="pp">We enjoy working with our clients. We are friendly and approachable. We take the time to understand your business and what is important to it and you. Plus, we’re practical and knowledgeable. So together, we will agree on the objective, we’ll talk at all critical stages and work hard to achieve a smart, cost conscious andpositive outcome
+</p>
+<br> 
+<br> 
+
+<h3 id="heff">Exceptional Value</h3>
+    <p id="pp">Value is defined as the worth of something compared to the price paid for it. Your measurement of value matters to us. With an unbridled dedication to professional integrity, we provide IP solutions that enhance your business strategies at anexceptional value. </p>
+<div id="imagee"><img src="images/hsclogo.png"></div>
+</div>
+  </div>  
+  <div id="TMS">
+  <form id="formx" method="post" action="tmi.php" enctype="multipart/form-data">
+  <br><br>
+    <input type="text" name="ytmname" placeholder="Your Name" id="nams" required onkeydown="fieldcheck()"  onchange="return validation()" Onkeyup="discon()"><span id="fi5"></span>
+    <input type="text" name="yatmname" placeholder="Your Address" id="nams" required onkeydown="fieldcheck()"  onchange="return validation()" Onkeyup="discon()"><span id="fi5"></span>
+    <input type="text" name="yeamil" placeholder="Your Email" id="nams" required onkeydown="fieldcheck()"  onchange="return validation()" Onkeyup="discon()"><span id="fi5"></span>
+    <input type="text" name="ycont" placeholder="Contact" id="nams" required onkeydown="fieldcheck()"  onchange="return validation()" Onkeyup="discon()"><span id="fi5"></span> 
+    <select id="optioneeee" name="logotype" onchange="fieldcheck()"  onclick="return validation()" ><option value="Select Logo Type">Select Proprietorship</option><option>Sole Proprietor</option><option>Partnership</option><option>Private Limited</option></select>
+    <input type="text" name="tmname" placeholder="Enter The TradeMark Name" id="nams" required onkeydown="fieldcheck()"  onchange="return validation()" Onkeyup="discon()"><span id="fi5"></span>
+    <select id="optioneeee" name="logotype" onchange="fieldcheck()"  onclick="return validation()" ><option value="Select Logo Type">Select Logo Type</option><option>Logo</option><option>Logo+Text</option><option>Text</option></select>
+    <input  required type="date" name="ydate"/>
+    <div id="godSer">
+    &nbsp&nbsp&nbsp&nbsp&nbsp Goods:<input type="radio" name="choice" id="rad" onclick="newfield()" Checked> Services:<input type="radio" name="choice" id="rad" onclick="newfield()"  onclick="return validation()">
+    </div>
+    <div id="gg" >
+    </div>
+   <div id="kk" value="choice" >
+   </div>
+   <div id="kkk" value="choice" >
+ 
+   </div>
+
+   <br>
+    <input type="submit" name="submits"  id="logbb" onclick="return validation()">
+   <input type="file" name="uploadfile" id="files" hidden>   
+    <button  id="uplo" onclick="upclick()"> Upload Logo</button>
+</form>
+
+  </div>  
+</body>
+</html>
